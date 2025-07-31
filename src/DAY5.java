0db7c1f25aed06a914f4039e15992ffd063f4b1c
SOUMYA PRATAP SINGH <soumyapratapsingh0001@gmail.com>
2025-07-31 13:34:27 +0530

    Solved day 5 challenge . Given an array arr. Your task is to find the minimum number in the array in the array. approach used = linear search.

--- src/DAY5.java
+++ src/DAY5.java
@@ -1,7 +1,28 @@
 import com.sun.jdi.PathSearchingVirtualMachine;
 
 public class DAY5 {
-    public static void main(String[] args) {
+           /*TOPIC = ARRAYS.
+     DAY 5 CHALLENGE :  FIND MISSING NUMBER IN THE ARRAY.
+    PROBLEM STATEMENTS : Given an array arr. Your task is to find the MISSING number in that array..
+      PLATFORM : GFG */
 
+    public static void main(String[] args) {
+    }
+    public int missingNo(int[] arr)
+    {
+        int n = arr.length+1;
+
+        for(int i = 0; i<arr.length; i++)
+        {
+            if(arr[i] != i+1)
+            {
+                return i+1;
+            }
+        }
+
+        return n;
     }
 }
+
+/*Time Complexity = o(n)
+Space complexity = o(1)*/
